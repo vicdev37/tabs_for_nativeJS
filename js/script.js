33024139e7b07201cb51fe3fde3c6fdb95640de3
Victor <justicejesus1237@gmail.com>
2019-03-14 19:30:02 +0300

    modal and lesson  done

--- js/script.js
+++ js/script.js
@@ -90,6 +90,42 @@ window.addEventListener('DOMContentLoaded', function() {
 	}
 	setClock('timer', deadline);	 	
 	
+
+	//Modal
+
+	let more = document.querySelector('.more'),
+		
+		overlay = document.querySelector('.overlay'),
+		close = document.querySelector('.popup-close');
+
+
+	
+	more.addEventListener('click', function() {
+		overlay.style.display = 'block';
+		this.classList.add('more-splash');
+		document.body.style.overflow = 'hidden';
+	});
+
+	close.addEventListener('click', function() {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = '';
+	});
+
+	let more2 = Array.from(document.querySelectorAll('.description-btn'));
+	
+
+	more2.forEach(el => {
+		el.addEventListener('click', function (event) {
+			overlay.style.display = 'block';
+			this.classList.add('more-splash');
+			document.body.style.overflow = 'hidden';
+			console.log(this);
+		});
+	})
+
+
+			
 });
 
 
---
+++ second_js.js
@@ -0,0 +1,18 @@
+let age = document.getElementById('age');
+
+
+function showUser(surname, name) {
+  this.surname = 'Иванов';
+  this.name = 'Виктор';
+  this.value = 30;
+  this.bingo = function () {
+   
+  };
+   alert("Пользователь " + this.surname + " " + this.name + ", его возраст " + this.value);
+}
+
+// let victor = new User('Victor');
+
+// console.log(victor);
+
+showUser();
